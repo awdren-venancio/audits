505a0dd7004e0119f6a8bdb2a112c1cc3bf79a85
Moysés Oliveira <contato@moysesoliveira.com.br>
2024-05-04 22:03:26 -0300

    init

---
+++ PlatformXMLBuilder/XMLBuilder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PlatformXMLBuilder;
+
+class XMLBuilder
+{
+
+    /**
+     * @param $jsonPath
+     * @return void
+     */
+    public static function fromJson($jsonPath):void
+    {
+        echo 'jsonPath: ' . $jsonPath . PHP_EOL;
+    }
+}
